a22dc22ecaf5d7ce999ce35f400dadef1c810671
phomemyintmo <phomemyintmo012@gmail.com>
2026-06-21 12:14:37 +0630

    update chat layout

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,11 +1,11 @@
 export default function Chat() {
   return (
-    <div className="mx-auto max-w-6xl p-4">
+    <div className="py-20 p-2 ">
       <iframe
         src="https://personal-ai-chatbot-alpha.vercel.app"
         title="AI Chat"
         className="w-full h-[90vh] rounded-xl border"
       />
-    </div>
+      </div>
   );
 }
